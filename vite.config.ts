fdc140ae1b62a31811e912ff36307d16bfcdae3b
Shan <t111ac1017@ntut.org.tw>
2026-03-25 19:48:21 +0800

    vite.config.ts更新

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,29 @@
 import { fileURLToPath, URL } from 'node:url'
-
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import vueDevTools from 'vite-plugin-vue-devtools'
+import Sitemap from 'vite-plugin-sitemap' // 1. 引入外掛
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [
     vue(),
     vueDevTools(),
+    // 2. 設定 Sitemap
+    Sitemap({
+      hostname: 'https://ruisse.cc/',
+      dynamicRoutes: [
+        '/',
+        '/about',
+        '/testimonials',
+        '/work/outdoor',
+        '/work/modelcard',
+        '/work/light',
+        '/work/exquisite',
+        '/work/birthday',
+        '/work/customized',
+      ],
+    }),
   ],
   resolve: {
     alias: {
